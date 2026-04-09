87f2d89463e081437b274d7e5619436afeea73cc
yoshi <99410296+Hix1337@users.noreply.github.com>
2026-04-09 14:09:35 +0300

    alr

--- Amalgam/src/Hooks/CBasePlayer_PostThink.cpp
+++ Amalgam/src/Hooks/CBasePlayer_PostThink.cpp
@@ -1,4 +1,5 @@
 #include "../SDK/SDK.h"
+#include "../Features/EnginePrediction/EnginePrediction.h"
 
 MAKE_SIGNATURE(CBasePlayer_PostThink, "client.dll", "48 89 5C 24 10 57 48 83 EC 20 48 8B 1D ? ? ? ? 48 8B F9", 0x0);
 
