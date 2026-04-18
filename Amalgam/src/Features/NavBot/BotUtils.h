15d02064b3b31a8829632fe416a51bfe2451aaff
vel <dobryfraudzichaxd@proton.me>
2026-04-18 06:24:26 +0200

    lol forgot to select it again

--- Amalgam/src/Features/NavBot/BotUtils.h
+++ Amalgam/src/Features/NavBot/BotUtils.h
@@ -45,12 +45,15 @@ private:
 		float m_flLastSeen = 0.f;
 		Vec3 m_vLastPos = {};
 		float m_flNextOffset = 1.f;
+		float m_flAcquireDuration = 0.f;
+		float m_flEnemyBlend = 0.f;
 		float m_flPhase = 0.f;
 		float m_flGlanceDuration = 0.f;
 		float m_flNextGlance = 0.f;
 		bool m_bInitialized = false;
 		bool m_bGlancing = false;
 		Timer m_tOffsetTimer = {};
+		Timer m_tAcquireTimer = {};
 		Timer m_tGlanceTimer = {};
 		Timer m_tGlanceCooldown = {};
 
